b9fa08b673e35fc4f638e17098d6d108e9120012
Tito <hveas10@gmail.com>
2020-09-21 19:10:07 -0300

    se agrego bootstrap,componentes: navbar e inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { InicioComponent } from './screens/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
